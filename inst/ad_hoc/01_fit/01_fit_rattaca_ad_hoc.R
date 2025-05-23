f2715f380c8d45b0624744be3f896f9826d46ef1
Benjamin Johnson <bbjohnson@health.ucsd.edu>
2025-05-23 14:33:00 -0700

    updated ad-hoc example

--- inst/ad_hoc/01_fit/01_fit_rattaca_ad_hoc.R
+++ inst/ad_hoc/01_fit/01_fit_rattaca_ad_hoc.R
@@ -121,10 +121,6 @@ cat('generation:', generation, '\n')
 #################################################
 
 
-# # get all RFIDs to use in the test set
-# test_rfids <- read.csv(rfid_list)$rfid
-# test_rfids_file <- format_ids_file(test_rfids, test_data_dir, paste0(trait, '_test_rfids'))
-
 # write phenotyped rfids to a file to enable subsetting using Plink
 phtyped_rfids_file <- get_phenotyped_ids(
     phenotype_data = train_pheno_file, 
@@ -135,75 +131,11 @@ phtyped_rfids_file <- get_phenotyped_ids(
 # make the phenotype training dataset
 pheno_train <- prep_trait_data(train_pheno_file, 'rfid', trait, 'numeric')
 
-
-get_common_snpset2 <- function(train_genotypes, test_genotypes, out_prefix=NULL) {
-    
-    if (length(train_genotypes) == 1 && is.character(train_genotypes) && file.exists(paste0(train_genotypes, '.bim'))) {
-        # read in snp if provided from a plink bim file
-        all_train_snps <- genio::read_bim(paste0(train_genotypes, '.bim'))
-        all_train_snps <- all_train_snps$id
-    } else if (length(train_genotypes) == 1 && is.character(train_genotypes) && 
-              grepl('.bpar$', train_genotypes) && file.exists(train_genotypes)) {
-        # read in snps if provided from a bpar file
-        all_train_snps <- get_snps_from_bpar(train_genotypes)
-    } else if (!is.list(train_genotypes) && length(train_genotypes) > 1) {
-        # keep snps as-is if provided as a vector of variant IDs
-        all_train_snps <- train_genotypes
-    } else if (is.list(train_genotypes) && 'geno_file' %in% names(train_genotypes)) {
-        # extract snps from genotype data if provided as a rattaca dataset
-        all_train_snps <- colnames(train_genotypes$geno)
-    } else if (is.matrix(train_genotypes)) {
-        # extract snps from column names if provided as a genotype matrix
-        all_train_snps <- colnames(train_genotypes)
-    } else {
-        cat('Check train_genotypes format: \n')
-        print(str(train_genotypes))
-        stop("Unrecognized format for train_genotypes")
-    }   
-
-    if (length(test_genotypes) == 1 && is.character(test_genotypes) && file.exists(paste0(test_genotypes, '.bim'))) {
-        # read in snp if provided from a plink bim file
-        all_test_snps <- genio::read_bim(paste0(test_genotypes, '.bim'))
-        all_test_snps <- all_test_snps$id
-    } else if (length(test_genotypes) == 1 && is.character(test_genotypes) && 
-              grepl('.bpar$', test_genotypes) && file.exists(test_genotypes)) {
-        # read in snps if provided from a bpar file
-        all_test_snps <- get_snps_from_bpar(test_genotypes)
-    } else if (!is.list(test_genotypes) && length(test_genotypes) > 1) {
-        # keep snps as-is if provided as a vector of variant IDs
-        all_test_snps <- test_genotypes
-    } else if (is.list(test_genotypes) && 'geno_file' %in% names(test_genotypes)) {
-        # extract snps from genotype data if provided as a rattaca dataset
-        all_test_snps <- colnames(test_genotypes$geno)
-    } else if (is.matrix(test_genotypes)) {
-        # extract snps from column names if provided as a genotype matrix
-        all_test_snps <- colnames(test_genotypes)
-    } else {
-        cat('Check test_genotypes format: \n')
-        print(str(test_genotypes))
-        stop("Unrecognized format for test_genotypes")
-    }   
-
-    # save all snps common to train/test sets from which to sample a common set of snps
-    all_snps <- intersect(all_test_snps, all_train_snps)
-    
-    out <- list(snps = all_snps, file = NULL)
-
-    # save a plink-formatted variant list
-    if (!is.null(out_prefix)) {
-        train <- basename(train_genotypes)
-        test <- basename(test_genotypes)
-        outfile <- paste0(out_prefix, '_common_snps')
-        writeLines(all_snps, outfile)
-        out$file <- outfile
-    }
-
-    return(out)
-}
-
 # get the common snp set between training and test genotype data
-all_snps <- get_common_snpset2(train_geno_input, test_geno_input, out_prefix=file.path(snp_dir, trait))
-
+all_snps <- get_common_snpset(
+    train_geno_input, 
+    test_geno_input, 
+    out_prefix = file.path(snp_dir, trait))
 
 # make the base training dataset
 printout('Producing base training genotype dataset')
@@ -308,142 +240,16 @@ if (sample_type == 'ldprune') {
     # fit a model to the entire training dataset using the best-performing SNP sample
     train_fit <- fit(train_dat$pheno, train_dat$geno)
 
-}
-
-write_pars2 <- function(
-    trait, trait_file, genotype_prefix, pars, filename,
-    genotype_source = NULL, # the training geno input file (eg. round 10.5)
-    trait_source = NULL, # the pheno datafile used to produce the trait_file
-    trait_variable = NULL # the official trait variable name from trait_source
-) {
-    META_PREFIX <- "##"
-    META_KEY_VAL_DELIM <- "="
-    META_VALID_KEY_REGEX <- ".+"
-    META_VALID_VAL_REGEX <- ".+"
-    RECORD_DELIM <- "\n"
-    FIELD_DELIM <- ","
-    ENCODING <- "UTF-8"
-    HEADER_PREFIX <- "#"
-
-    sess_info <- utils::sessionInfo()
-
-    # append .bpar file extension to output file name if needed
-    if (!grepl('.bpar$', filename)) {
-        filename <- paste0(filename, '.bpar')
-    }
-
-    fconn <- file(description=filename,
-                  open="wt",
-                  encoding=ENCODING)
-
-    writeLines(c(paste(paste0(META_PREFIX,"date"),
-                       Sys.time(),
-                       sep=META_KEY_VAL_DELIM),
-                 paste(paste0(META_PREFIX, "user"),
-                       Sys.getenv("USER"),
-                       sep=META_KEY_VAL_DELIM),
-                 paste(paste0(META_PREFIX, "trait_name"),
-                       trait,
-                       sep=META_KEY_VAL_DELIM),
-                 paste(paste0(META_PREFIX,"trait_file"),
-                       trait_file,
-                       sep=META_KEY_VAL_DELIM),
-                 paste(paste0(META_PREFIX, "trait_source"),
-                       trait_source,
-                       sep=META_KEY_VAL_DELIM),
-                 paste(paste0(META_PREFIX, "trait_source_variable"),
-                       trait_variable,
-                       sep=META_KEY_VAL_DELIM),
-                 paste(paste0(META_PREFIX, "genotypes_file"),
-                       genotype_prefix,
-                       sep=META_KEY_VAL_DELIM),
-                 paste(paste0(META_PREFIX, "genotypes_source"),
-                       genotype_source,
-                       sep=META_KEY_VAL_DELIM),
-                 paste(paste0(META_PREFIX,"R_version"),
-                       paste(sess_info$R.version$major,
-                             sess_info$R.version$minor,
-                             sep="."),
-                       sep=META_KEY_VAL_DELIM),
-                 paste(paste0(META_PREFIX, "platform"),
-                       sess_info$R.version$platform,
-                       sep=META_KEY_VAL_DELIM)),
-               con=fconn)
-
-    pkg_info <- vector(mode="character",
-                       length(utils::sessionInfo()$otherPkgs))
-    i <- 1
-    for (tmp_pkg_info in sess_info$otherPkgs)
-    {
-        tmp <- paste(tmp_pkg_info$Package,
-                     tmp_pkg_info$Version,
-                     sep=":")
-
-        pkg_info[i] <- paste(paste0(META_PREFIX, "package"),
-                             tmp,
-                             sep=META_KEY_VAL_DELIM)
-        i <- i + 1
-    }
-
-    writeLines(pkg_info, con=fconn)
-    
-    writeLines(c(paste(paste0(META_PREFIX, "n_snps"),
-                       length(pars$u),
-                       sep=META_KEY_VAL_DELIM),
-                 paste(paste0(META_PREFIX, "variance_u"),
-                       pars$Vu,
-                       sep=META_KEY_VAL_DELIM),
-                paste(paste0(META_PREFIX, "variance_e"),
-                       pars$Ve,
-                       sep=META_KEY_VAL_DELIM),
-                 paste(paste0(META_PREFIX, "log_likelihood"),
-                       pars$LL,
-                       sep=META_KEY_VAL_DELIM),
-                 paste(paste0(META_PREFIX, "intercept"),
-                       pars$beta,
-                       sep=META_KEY_VAL_DELIM),
-                 paste(paste0(META_PREFIX, "intercept_se"),
-                       pars$beta.SE,
-                       sep=META_KEY_VAL_DELIM),
-                 paste(paste0(META_PREFIX, "r_sq"),
-                       pars$r_sq,
-                       sep=META_KEY_VAL_DELIM),
-                 paste(paste0(META_PREFIX, "pearson_corr"),
-                       pars$pearson_corr,
-                       sep=META_KEY_VAL_DELIM),
-                 paste(paste0(META_PREFIX, "spearman_corr"),
-                       pars$spearman_corr,
-                       sep=META_KEY_VAL_DELIM)),
-               con=fconn)
-
-    writeLines(paste(paste0(HEADER_PREFIX, "var_id"),
-                     "random_effect",
-                     "random_effect_se",
-                     sep=FIELD_DELIM),
-               con=fconn)
-
-    for (uname in names(pars$u))
-    {
-        writeLines(paste(uname,
-                         pars$u[uname],
-                         pars$u.SE[uname],
-                         sep=FIELD_DELIM),
-                   con=fconn)
-    }
-    close(fconn)
-    cat('Model parameters written to', filename, '\n')
-
-}
+} # end of random model fits
 
 # save model parameters
 printout('Writing model parameters to file')
 trait_bpar_file <- file.path(trait_dir, paste0(trait, '.bpar')) # to save model parameters with trait results
 paramfiles <- c(trait_bpar_file, bpar_file)
 
-
 for (parfile in paramfiles) {
 
-    write_pars2(
+    write_pars(
         trait = trait, 
         trait_file = train_pheno_file, 
         genotype_prefix = train_dat$geno_file, 
@@ -459,33 +265,6 @@ for (parfile in paramfiles) {
 for (cv_outdir in c(trait_dir, trait_bpar_dir)) {
     save_cv_results(cv_out, cv_outdir)
     plot_kfold(cv_out, cv_outdir)
-}
-
-
-save_training_data <- function(bpar_file) {
-
-    pars <- read_pars(bpar_file)
-    
-    # get variant IDs used in the model
-    dat <- pars$data
-    snp_ids <- rownames(dat)
-
-    # get RFIDs and trait values used to train the model
-    trait <- pars$meta$trait_name
-    trait_var <- pars$meta$trait_source_variable
-    trait_dat <- pars$meta$trait_file
-    trait_dat <- read.csv(trait_dat)
-    trait_dat <- trait_dat[,c('rfid',trait)]
-    trait_dat <- trait_dat[complete.cases(trait_dat),]
-
-    # write variants and pheno data to files
-    outdir <- dirname(bpar_file)    
-    snp_file <- file.path(outdir, paste0(trait_var, '_train_snps'))
-    trait_file <- file.path(outdir, paste0(trait_var, '_train_pheno.csv'))
-    
-    writeLines(snp_ids, snp_file)
-    write.table(trait_dat, trait_file, sep=',', row.names=F, col.names=F, quote=F, na='')
-
 }
 
 # save training variants and pheno data with the .bpar file
